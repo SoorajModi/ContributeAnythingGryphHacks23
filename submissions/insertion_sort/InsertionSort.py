9e68ae20b39597854b760b7727b09965480dfef7
ConverseScholar <97484565+ConverseScholar@users.noreply.github.com>
2023-05-28 07:35:31 +0400

    InsertionSort demo in Python (#13)
    * contribution made
    
    * contribution made
    
    * contribution made
    
    * contribution made
    
    * updated contribution

---
+++ submissions/insertion_sort/InsertionSort.py
@@ -0,0 +1,14 @@
+def InsertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+a = [1, 4, 5, 2]
+print(InsertionSort(a))
+
+
